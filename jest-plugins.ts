e9b76ca4363406939af7848ab0546c264e66798a
fateh <kfateh2007@gmail.com>
2023-04-24 14:01:57 -0700

    Update jest-plugins.ts

--- jest-plugins.ts
+++ jest-plugins.ts
@@ -1,103 +1,80 @@
-const fs = require('fs');
-const path = require('path');
-const readline = require('readline');
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const inquirer = require('inquirer');
+const mkdirp = require('mkdirp');
 
-const moduleName = process.argv[2];
+module.exports = class extends Generator {
+  async prompting() {
+    const answers = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'moduleName',
+        message: 'Enter the name of the new module:',
+        validate: function (input) {
+          if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
+          else return 'Module name may only include letters, numbers, underscores and hashes.';
+        }
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description for the module:'
+      },
+      {
+        type: 'input',
+        name: 'author',
+        message: 'Enter the author name for the module:'
+      }
+    ]);
 
-if (!moduleName) {
-  console.error('Please specify a module name');
-  process.exit(1);
-}
+    this.props = answers;
+  }
 
-// Check if the module directory already exists
-const moduleDir = path.resolve(__dirname, '..', 'modules', moduleName);
-if (fs.existsSync(moduleDir)) {
-  console.error(`Module directory already exists: ${moduleName}`);
-  process.exit(1);
-}
+  writing() {
+    const { moduleName, description, author } = this.props;
 
-// Create the module directory
-fs.mkdirSync(moduleDir);
+    // Create the module directory
+    mkdirp.sync(`Modules/${moduleName}`);
 
-// Create the __tests__ directory
-const testsDir = path.resolve(moduleDir, '__tests__');
-fs.mkdirSync(testsDir);
+    // Create the necessary files
+    this.fs.copyTpl(
+      this.templatePath('service.config.ts'),
+      this.destinationPath(`Modules/${moduleName}/service.config.ts`),
+      { moduleName, description, author }
+    );
 
-// Create the data directory
-const dataDir = path.resolve(moduleDir, 'data');
-fs.mkdirSync(dataDir);
+    this.fs.copy(
+      this.templatePath('__tests__/index.test.ts'),
+      this.destinationPath(`Modules/${moduleName}/__tests__/index.test.ts`)
+    );
 
-// Create the docs directory
-const docsDir = path.resolve(moduleDir, 'docs');
-fs.mkdirSync(docsDir);
+    this.fs.copy(
+      this.templatePath('data/data.json'),
+      this.destinationPath(`Modules/${moduleName}/data/data.json`)
+    );
 
-// Create the reports directory
-const reportsDir = path.resolve(moduleDir, 'reports');
-fs.mkdirSync(reportsDir);
+    this.fs.copy(
+      this.templatePath('docs/README.md'),
+      this.destinationPath(`Modules/${moduleName}/docs/README.md`)
+    );
 
-// Create the actions.ts file
-const actionsFile = path.resolve(moduleDir, 'actions.ts');
-fs.writeFileSync(actionsFile, '');
+    this.fs.copy(
+      this.templatePath('reports/index.html'),
+      this.destinationPath(`Modules/${moduleName}/reports/index.html`)
+    );
 
-// Create the objects.ts file
-const objectsFile = path.resolve(moduleDir, 'objects.ts');
-fs.writeFileSync(objectsFile, '');
+    this.fs.copy(
+      this.templatePath('actions.ts'),
+      this.destinationPath(`Modules/${moduleName}/actions.ts`)
+    );
 
-// Prompt the user for a description and author
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-rl.question('Enter a description for the new module: ', (description) => {
-  rl.question('Enter the author of the module: ', (author) => {
-    rl.close();
+    this.fs.copy(
+      this.templatePath('objects.ts'),
+      this.destinationPath(`Modules/${moduleName}/objects.ts`)
+    );
+  }
 
-    // Create the service.config.ts file
-    const configData = `{
-      "name": "${moduleName}",
-      "description": "${description}",
-      "ignoredTests": [],
-      "createDate": "${new Date().toISOString()}",
-      "author": "${author}",
-      "testsDir": "__tests__"
-    }`;
-    const configFile = path.resolve(moduleDir, 'service.config.ts');
-    fs.writeFileSync(configFile, configData);
-
-    console.log(`Module created successfully: ${moduleName}`);
-  });
-});
-
-import fs from 'fs';
-import path from 'path';
-
-const createModule = async (moduleName: string) => {
-  try {
-    const modulePath = path.join(__dirname, '../modules', moduleName);
-
-    if (fs.existsSync(modulePath)) {
-      console.error(`Module "${moduleName}" already exists`);
-      return;
-    }
-
-    fs.mkdirSync(modulePath);
-    fs.mkdirSync(path.join(modulePath, '__tests__'));
-    fs.mkdirSync(path.join(modulePath, 'data'));
-    fs.mkdirSync(path.join(modulePath, 'docs'));
-    fs.mkdirSync(path.join(modulePath, 'reports'));
-    fs.writeFileSync(path.join(modulePath, 'actions.ts'), '');
-    fs.writeFileSync(path.join(modulePath, 'objects.ts'), '');
-    fs.writeFileSync(path.join(modulePath, 'service-config.ts'), `{
-  "name": "${moduleName}",
-  "description": "${await prompt('Enter module description')}",
-  "ignoredTests": [],
-  "createDate": "${new Date().toISOString()}",
-  "author": "${await prompt('Enter author name')}",
-  "testsDir": "__tests__"
-}`);
-    console.log(`Module "${moduleName}" created successfully`);
-  } catch (err) {
-    console.error(`Error creating module "${moduleName}":`, err);
+  end() {
+    this.log(chalk.green('New module created successfully!'));
   }
 };
-
