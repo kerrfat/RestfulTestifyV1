d8cf9d9d9aed99dc1808f0b1b09e9a9eb7842d4f
fateh <kfateh2007@gmail.com>
2023-04-24 10:08:33 -0700

    Update jest-plugins.ts

--- jest-plugins.ts
+++ jest-plugins.ts
@@ -1,107 +1,69 @@
-toHaveHeader: This matcher checks if the HTTP response has a specific header with the expected value. Here's an example implementation:
-javascript
-Copy code
-expect.extend({
-  toHaveHeader(received, headerName, expectedValue) {
-    const headerValue = received.headers[headerName.toLowerCase()];
-    const pass = headerValue === expectedValue;
-    if (pass) {
-      return {
-        message: () =>
-          `Expected HTTP response header '${headerName}' not to be '${expectedValue}', but received '${headerValue}'`,
-        pass: true,
-      };
-    } else {
-      return {
-        message: () =>
-          `Expected HTTP response header '${headerName}' to be '${expectedValue}', but received '${headerValue}'`,
-        pass: false,
-      };
-    }
-  },
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const moduleName = process.argv[2];
+
+if (!moduleName) {
+  console.error('Please specify a module name');
+  process.exit(1);
+}
+
+// Check if the module directory already exists
+const moduleDir = path.resolve(__dirname, '..', 'modules', moduleName);
+if (fs.existsSync(moduleDir)) {
+  console.error(`Module directory already exists: ${moduleName}`);
+  process.exit(1);
+}
+
+// Create the module directory
+fs.mkdirSync(moduleDir);
+
+// Create the __tests__ directory
+const testsDir = path.resolve(moduleDir, '__tests__');
+fs.mkdirSync(testsDir);
+
+// Create the data directory
+const dataDir = path.resolve(moduleDir, 'data');
+fs.mkdirSync(dataDir);
+
+// Create the docs directory
+const docsDir = path.resolve(moduleDir, 'docs');
+fs.mkdirSync(docsDir);
+
+// Create the reports directory
+const reportsDir = path.resolve(moduleDir, 'reports');
+fs.mkdirSync(reportsDir);
+
+// Create the actions.ts file
+const actionsFile = path.resolve(moduleDir, 'actions.ts');
+fs.writeFileSync(actionsFile, '');
+
+// Create the objects.ts file
+const objectsFile = path.resolve(moduleDir, 'objects.ts');
+fs.writeFileSync(objectsFile, '');
+
+// Prompt the user for a description and author
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
+rl.question('Enter a description for the new module: ', (description) => {
+  rl.question('Enter the author of the module: ', (author) => {
+    rl.close();
 
-toBeValidJSON: This matcher checks if the response body is a valid JSON string. Here's an example implementation:
-javascript
-Copy code
-expect.extend({
-  toBeValidJSON(received) {
-    let pass = true;
-    let error = null;
-    try {
-      JSON.parse(received);
-    } catch (e) {
-      pass = false;
-      error = e;
-    }
-    if (pass) {
-      return {
-        message: () => `Expected '${received}' not to be a valid JSON string`,
-        pass: true,
-      };
-    } else {
-      return {
-        message: () => `Expected '${received}' to be a valid JSON string, but received error: ${error}`,
-        pass: false,
-      };
-    }
-  },
-});
+    // Create the service.config.ts file
+    const configData = `{
+      "name": "${moduleName}",
+      "description": "${description}",
+      "ignoredTests": [],
+      "createDate": "${new Date().toISOString()}",
+      "author": "${author}",
+      "testsDir": "__tests__"
+    }`;
+    const configFile = path.resolve(moduleDir, 'service.config.ts');
+    fs.writeFileSync(configFile, configData);
 
-ation for a toHaveKeysAndValues matcher that checks if an object has certain keys and their corresponding values:
-
-javascript
-Copy code
-expect.extend({
-  toHaveKeysAndValues(received, expected) {
-    const receivedKeys = Object.keys(received);
-    const expectedKeys = Object.keys(expected);
-
-    const missingKeys = expectedKeys.filter((key) => !receivedKeys.includes(key));
-    const extraKeys = receivedKeys.filter((key) => !expectedKeys.includes(key));
-    const incorrectValues = expectedKeys.filter((key) => received[key] !== expected[key]);
-
-    const pass = missingKeys.length === 0 && extraKeys.length === 0 && incorrectValues.length === 0;
-
-    if (pass) {
-      return {
-        message: () =>
-          `Expected object ${JSON.stringify(received)} not to have keys and values ${JSON.stringify(
-            expected
-          )}, but it did`,
-        pass: true,
-      };
-    } else {
-      let errorMessage = "";
-      if (missingKeys.length > 0) {
-        errorMessage += `Expected object ${JSON.stringify(received)} to have keys ${JSON.stringify(
-          missingKeys
-        )}, but they were missing. `;
-      }
-      if (extraKeys.length > 0) {
-        errorMessage += `Expected object ${JSON.stringify(received)} not to have keys ${JSON.stringify(
-          extraKeys
-        )}, but they were present. `;
-      }
-      if (incorrectValues.length > 0) {
-        errorMessage += `Expected object ${JSON.stringify(received)} to have values ${JSON.stringify(
-          expected
-        )}, but the values were ${JSON.stringify(
-          received
-        )}. `;
-      }
-      return {
-        message: () => errorMessage,
-        pass: false,
-      };
-    }
-  },
-});
-You can use this matcher like this:
-
-php
-Copy code
-it("should have the correct keys and values", () => {
-  const obj = { name: "Alice", age: 25 };
-  expect(obj).toHaveKeysAndValues({ name: "Alice", age: 25 });
+    console.log(`Module created successfully: ${moduleName}`);
+  });
 });
